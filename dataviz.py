5c4c19682ba928983e53a7ccf62d6b7b2ed76f17
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2023-02-06 21:17:34 -0500

    ChatIA data viz script
    Python/matplotlib script made using chatAI. Prompts for filename (csv) x variable, y bariable and plot type

---
+++ dataviz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+# Get the file name, X variable, and Y variable from the command line
+file_name = sys.argv[1] 
+x_variable = sys.argv[2]
+y_variable = sys.argv[3]
+z_variable = sys.argv[4] # kind of plot to make
+
+# Read CSV file into a DataFrame
+df = pd.read_csv(file_name)
+
+# Plot the X and Y variables
+# plt.plot(df[x_variable], df[y_variable])
+df.plot(kind=z_variable, x=x_variable, y=y_variable)
+plt.xlabel(x_variable)
+plt.ylabel(y_variable)
+plt.title(f"{y_variable} vs {x_variable}")
+plt.savefig('books_read.png')
+# Show the plot
+# plt.show()
